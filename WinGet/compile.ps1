fe1d8b759dd233f367a994dc7630636099812de4
Russell Gallinera <russell.gallinera@mmf.mb.ca>
2025-08-12 15:10:25 -0500

    Updated directory structure, added new app

--- WinGet/compile.ps1
+++ WinGet/compile.ps1
@@ -2,4 +2,4 @@ $scriptVersion = "1.0.0"
 $sourceFolder = "$PSScriptRoot\..\..\WinGet"
 $destinationFolder = ".\$scriptVersion"
 $setupFile = "$sourceFolder\install.cmd"
-.\IntuneWinAppUtil.exe -c $sourceFolder -s $setupFile -o $destinationFolder -q
+..\IntuneWinAppUtil.exe -c $sourceFolder -s $setupFile -o $destinationFolder -q
